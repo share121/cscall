b85571be94405a2d297636f7f4de0cb36a9d073a
share121 <me@s121.top>
2026-01-14 13:28:23 +0800

    pref: 把所有 EventType 后移一位，防止端口扫描时填 0 导致的 Encrypted 事件反复触发

--- src/lib.rs
+++ src/lib.rs
@@ -16,24 +16,24 @@ pub const HEARTBEAT_MS: u64 = 10000;
 #[allow(non_snake_case, non_upper_case_globals)]
 pub mod EventType {
     /// SessionKey(PlainText + Count) + Uid + Encrypted
-    pub const Encrypted: u8 = 0;
+    pub const Encrypted: u8 = 1;
 
     /// 63个0 + Hello
     /// 填充 63 个 0 防止利用服务器来反射攻击
-    pub const Hello: u8 = 1;
+    pub const Hello: u8 = 2;
 
     /// ServerSalt + AckHello
-    pub const AckHello: u8 = 2;
+    pub const AckHello: u8 = 3;
 
     /// ServerKey(SessionKey + TimeStamp + Uid) + Connect
-    pub const Connect: u8 = 3;
+    pub const Connect: u8 = 4;
 
     /// SessionKey(Uid) + AckConnect
-    pub const AckConnect: u8 = 4;
+    pub const AckConnect: u8 = 5;
 
     /// SessionKey(Count) + Uid + Heartbeat
-    pub const Heartbeat: u8 = 5;
+    pub const Heartbeat: u8 = 6;
 
     /// SessionKey(Count) + Uid + AckHeartbeat
-    pub const AckHeartbeat: u8 = 6;
+    pub const AckHeartbeat: u8 = 7;
 }
